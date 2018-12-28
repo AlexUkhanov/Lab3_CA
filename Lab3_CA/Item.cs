979ad6dc82e5a30cf1dbbb554fd5b7c4b5ccaa57
Alexey Ukhanov <alexey-ukhanov@yandex.ru>
2018-12-28 17:14:30 +0300

    Created class "Item"

---
+++ Lab3_CA/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3_CA
+{
+    class Item
+    {
+        public double Cost { get; set; }
+        public double Weigth { get; set; }
+        public string Name { get; set; }
+
+        public Item(string name_in,double weigth_in,double cost_in)
+        {
+            Name = name_in;
+            Weigth = weigth_in;
+            Cost = cost_in;
+        }
+    }
+}
